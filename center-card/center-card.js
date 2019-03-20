38cccc84b8ddfbf2717075bb6e08bc6f3e19ace1
assur93 <47425249+assur93@users.noreply.github.com>
2019-03-20 11:36:13 +0100

    Add files via upload

---
+++ center-card/center-card.js
@@ -0,0 +1,88 @@
+class CenterCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+  setConfig(config) {
+
+    const root = this.shadowRoot;
+    if (root.lastChild) root.removeChild(root.lastChild);
+    const cardConfig = Object.assign({}, config);
+    if (!cardConfig.scale) cardConfig.scale = "50px";
+    if (!cardConfig.from) cardConfig.from = "left";
+    const card = document.createElement('ha-card');
+    const content = document.createElement('div');
+    content.id = "value"
+    const title = document.createElement('div');
+    title.id = "title"
+    title.textContent = cardConfig.title;
+    const style = document.createElement('style');
+    style.textContent = `
+      ha-card {
+        text-align: center;
+        --bignumber-fill-color: var(--label-badge-blue);
+        --bignumber-percent: 100%;
+        --bignumber-direction: ${cardConfig.from};
+        --base-unit: ${cardConfig.scale};
+        padding: calc(var(--base-unit)*0.6) calc(var(--base-unit)*0.3);
+        background: linear-gradient(to var(--bignumber-direction), var(--paper-card-background-color) var(--bignumber-percent), var(--bignumber-fill-color) var(--bignumber-percent));
+      }
+      #title {
+        font-size: calc(var(--base-unit) * 1.3);
+        line-height: calc(var(--base-unit) * 1.3);
+        color: var(--primary-text-color);
+      }
+      #value {
+        font-size: calc(var(--base-unit) * 0.005);
+        line-height: calc(var(--base-unit) * 0.005);
+        color: rgb(255,255,255);
+      }
+    `;
+	card.appendChild(title);
+	card.appendChild(content);
+    card.appendChild(style);
+    root.appendChild(card);
+    this._config = cardConfig;
+  }
+
+  _fire(type, detail, options) {
+    const node = this.shadowRoot;
+    options = options || {};
+    detail = (detail === null || detail === undefined) ? {} : detail;
+    const event = new Event(type, {
+      bubbles: options.bubbles === undefined ? true : options.bubbles,
+      cancelable: Boolean(options.cancelable),
+      composed: options.composed === undefined ? true : options.composed
+    });
+    event.detail = detail;
+    node.dispatchEvent(event);
+    return event;
+  }
+
+  _computeSeverity(stateValue, sections) {
+    const numberValue = Number(stateValue);
+    let style;
+    sections.forEach(section => {
+      if (numberValue <= section.value && !style) {
+        style = section.style;
+      }
+    });
+    return style || 'var(--label-badge-blue)';
+  }
+
+  _translatePercent(value, min, max) {
+    return 100-100 * (value - min) / (max - min);
+  }
+
+  set hass(hass) {
+    const config = this._config;
+    const root = this.shadowRoot;
+    root.lastChild.hass = hass;
+  }
+
+  getCardSize() {
+    return 1;
+  }
+}
+
+customElements.define('center-card', CenterCard);
